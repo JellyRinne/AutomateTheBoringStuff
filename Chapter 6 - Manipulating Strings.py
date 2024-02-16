225c3210f019bc7ac83eacbc2fd146990910fa15
JellyRinne <jelly.rinne@icloud.com>
2024-02-15 23:54:49 -0800

    more chapter 6

--- Chapter 6 - Manipulating Strings.py
+++ Chapter 6 - Manipulating Strings.py
@@ -45,3 +45,96 @@ if 'Hello' in spam:
         print(buzz)
 else:
     print('wtf?')
+
+print()
+
+def stringspam():
+    name = 'Al'
+    age = '4000'
+    print('Hello, my name is %s. I am %s years old' % (name, age))
+
+stringspam()
+
+print()
+
+def stringspam2():
+    name = 'Mike'
+    age = '30'
+    print(f'My name is {name}. I am {age} years old.')
+
+stringspam2()
+
+print()
+
+def stringspam3():
+    spam = "hello, world!"
+    print(spam.upper())
+    print(spam.lower())
+
+stringspam3()
+
+print()
+
+def stringspam4():
+    print('How are you?')
+    feeling = input()
+    if feeling.lower().upper().lower() == 'great':
+        print('I feel great too')
+    else:
+        print('I hope the rest of your day is good')
+
+stringspam4()
+
+print()
+
+def stringspam5():
+    print('How are you?')
+    feeling = input()
+    if feeling.isupper() == True:
+        print('Calm down, jackass.')
+    else:
+        print('kay thanks')
+
+stringspam5()
+
+print()
+
+def stringspam6():
+    print('enter some text')
+    text = input()
+    if text.isalpha():
+        print('isAlpha')
+    elif text.isalnum():
+        print('isAlNum')
+    elif text.isdecimal():
+        print('isDecimal')
+    elif text.isspace():
+        print('isSpace')
+    elif text.istitle():
+        print('isTitle')
+    else:
+        print('wtf?')
+
+stringspam6()
+
+print()
+
+def stringspam7():
+    while True:
+        print('Enter your age:')
+        age = input()
+        if age.isdecimal():
+            break
+        print('Please enter a number for your age.')
+
+    while True:
+        print('Select a new password (letters and numbers only):')
+        password = input()
+        if password.isalnum():
+            break
+        print('Passwords can only have letters and numbers.')
+
+stringspam7()
+
+print()
+
